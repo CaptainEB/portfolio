5266b89eaf37376bd081ae293f70e5b26ad6527d
Sam <Sambose426@yahoo.com>
2023-12-02 03:04:32 -0700

    added notable skills section below hero in the main page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,11 @@
 import { londrina, majorMono } from '@/components/Fonts';
 import Image from 'next/image';
+import cssIcon from 'public/icons/css.svg';
+import expressIcon from 'public/icons/express.svg';
+import htmlIcon from 'public/icons/html.svg';
+import mongoIcon from 'public/icons/mongo.svg';
+import nextIcon from 'public/icons/nextjs.svg';
+import reactIcon from 'public/icons/react-icon.png';
 import heroImage from 'public/img/hero-image-1x1.jpg';
 import styles from './page.module.scss';
 
@@ -15,6 +21,14 @@ export default function Home() {
 					<h2>Developer | Engineer</h2>
 					<h2>FullStack</h2>
 				</div>
+			</section>
+			<section className={styles.notableSkills}>
+				<Image src={reactIcon} alt="React Icon" />
+				<Image src={nextIcon} alt="NextJS Icon" />
+				<Image src={mongoIcon} alt="MongoDB Icon" />
+				<Image src={expressIcon} alt="ExpressJS Icon" />
+				<Image src={cssIcon} alt="CSS Icon" />
+				<Image src={htmlIcon} alt="HTML Icon" />
 			</section>
 		</main>
 	);
