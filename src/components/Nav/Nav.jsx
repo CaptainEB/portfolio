88155198539c577e933bb64570ba6ee3ff279fcc
Sam <Sambose426@yahoo.com>
2023-12-23 02:11:55 -0700

    finished styling the nav button

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -55,9 +55,14 @@ export default function Nav() {
 				</ul>
 			</div>
 			<button className={styles.navBtn} onClick={showNav} ref={navbtn} aria-expanded="false">
-				<div className={styles.top}></div>
+				{/* <div className={styles.top}></div>
 				<div className={styles.middle}></div>
-				<div className={styles.bottom}></div>
+				<div className={styles.bottom}></div> */}
+				<svg viewBox="0 0 100 100" width="50" height="50">
+					<rect className={`${styles.top} ${styles.line}`} width="80" height="10" x={10} y={25} rx={5}></rect>
+					<rect className={`${styles.middle} ${styles.line}`} width="80" height="10" x={10} y={50} rx={5}></rect>
+					<rect className={`${styles.bottom} ${styles.line}`} width="80" height="10" x={10} y={75} rx={5}></rect>
+				</svg>
 			</button>
 		</nav>
 	);
