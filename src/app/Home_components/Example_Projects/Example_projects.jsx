91dd414d72bc43efdde11ebe6faeece2d47a1e00
Sam <Sambose426@yahoo.com>
2023-12-28 03:24:40 -0700

    finished the home page styling

--- src/app/Home_components/Example_Projects/Example_projects.jsx
+++ src/app/Home_components/Example_Projects/Example_projects.jsx
@@ -1,16 +1,33 @@
 import { majorMono } from '@/components/Fonts';
 import Image from 'next/image';
-import project2 from 'public/img/binge.png';
-import project1 from 'public/img/taco-tap.png';
+import Link from 'next/link';
+import projects from 'projects-data.json';
 import styles from './Example_projects.module.scss';
 
 export default function Example_projects() {
+	const { tacoTap, binge } = projects;
 	return (
 		<section className={styles.exampleProjects}>
 			<h2 className={majorMono.className}>Example Projects</h2>
 			<div className={styles.projects}>
-				<Image src={project1} alt="Project 1" />
-				<Image src={project2} alt="Project 2" />
+				<div className={styles.project}>
+					<div className={styles.imgWrapper}>
+						<Image className={styles.projectImage} src={tacoTap.image} alt="Project 1" fill />
+					</div>
+					<div className={styles.projectLinks}>
+						<Link href={tacoTap.liveUrl}>Live URL</Link>
+						<Link href={tacoTap.githubUrl}>Github</Link>
+					</div>
+				</div>
+				<div className={styles.project}>
+					<div className={styles.imgWrapper}>
+						<Image className={styles.projectImage} src={binge.image} alt="Project 2" fill />
+					</div>
+					<div className={styles.projectLinks}>
+						<Link href={binge.liveUrl}>Live URL</Link>
+						<Link href={binge.githubUrl}>Github</Link>
+					</div>
+				</div>
 			</div>
 		</section>
 	);
