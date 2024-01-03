cb37f57c5c31bc3486b698f63f28cd1989bfe05f
Sam <Sambose426@yahoo.com>
2024-01-02 23:54:13 -0700

    added vercel speed insights

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,7 @@
 import Footer from '@/components/Footer';
 import Nav from '@/components/Nav';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import { Inter } from 'next/font/google';
 import './globals.scss';
 
@@ -19,6 +20,7 @@ export default function RootLayout({ children }) {
 				<main className="main">{children}</main>
 				<Footer />
 				<Analytics />
+				<SpeedInsights />
 			</body>
 		</html>
 	);
